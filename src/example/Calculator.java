93291268311e86ccd74d3239bb0e5c6184c43397
t2025-m0040 <e0321e@nate.com>
2026-01-12 16:33:34 +0900

    if 반복문에서 else {} 제거

--- src/example/Calculator.java
+++ src/example/Calculator.java
@@ -30,9 +30,8 @@ public class Calculator {
                     if (num2 == 0) {
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                         continue;
-                    } else {
-                        result = num1 / num2;
                     }
+                    result = num1 / num2;
                     break;
                 default:
                     System.out.println("올바른 연산자가 아닙니다.");
