15db9d8143ea3270db9617dab79251d7e1095fc3
t2025-m0040 <e0321e@nate.com>
2026-01-21 12:22:26 +0900

    feat: Step1 완료 및 Step2(Calculator 분리/예외처리) 일부 구현

---
+++ src/calculator/App.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello, Calculation!!");
+
+        boolean run = true;
+        while (run) {
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                if (!sc.hasNextInt()) {
+                    System.out.println("----숫자만 입력하세요----!");
+                    sc.next();
+                    continue;
+                }
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                if (!sc.hasNextInt()) {
+                    System.out.println("----숫자만 입력하세요----!");
+                    sc.next();
+                    continue;
+                }
+                int num2 = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+
+                int result = calculator.calculate(num1, num2, operator);
+                System.out.println("결과: " + result);
+                System.out.println();
+                System.out.println("더 계산하시겠습니까?(exit 입력시 종료)");
+                String exit = sc.next();
+                if ("exit".equals(exit)) {
+                    System.out.println("계 산 기 프 로 그 램 종 료 ^_^");
+                    run = false;
+                }
+            }
+            catch (ArithmeticException | IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
+        sc.close();
+    }
+}
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Integer> results = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator){
+
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("----나눗셈 연산에서 분모에 0이 입력될 수 없습니다.----");
+                }
+                result = num1 / num2;
+                break;
+
+            default:
+                throw new IllegalArgumentException("----올바른 연산자가 아닙니다 (+, -, *, /)----");
+        }
+        results.add(result);
+        return result;
+    }
+}
