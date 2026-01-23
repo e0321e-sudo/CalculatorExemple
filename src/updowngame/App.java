f8c7073745035c81df76af09ed70b6e7b2ac700f
t2025-m0040 <e0321e@nate.com>
2026-01-23 22:44:07 +0900

    fix: UpDown 게임 반복 흐름 수정 및 입력 처리 보완

--- src/updowngame/App.java
+++ src/updowngame/App.java
@@ -11,42 +11,46 @@ public class App {
 
         System.out.println("==== UP DOWN GAME START! ====");
 
-        int ranNum = random.nextInt(100) + 1;
-        System.out.println("랜덤 번호가 정해졌습니다.( 1 ~ 100 )");
-        System.out.println("              ⇩              ");
+        boolean replay = true;
+        while (replay) {
+            int ranNum = random.nextInt(100) + 1;
+            System.out.println("랜덤 번호가 정해졌습니다.( 1 ~ 100 )");
+            System.out.println("              ⇩              ");
 
-        boolean run1 = true;
-        while (run1) {
-            try {
-                System.out.print("번호 입력 > ");
-                int num = sc.nextInt();
-                if (num == ranNum) {
-                    System.out.println("----축하합니다! 정답입니다 *^^*----");
-                    run1 = false;
-                } else if (num < ranNum) {
-                    System.out.println("======> UP!!   ");
+            boolean run = true;
+            while (run) {
+                try {
+                    System.out.print("번호 입력 > ");
+                    int num = sc.nextInt();
+                    sc.nextLine();
+                    if (num < 1 || num > 100) {
+                        throw new IllegalArgumentException("1~100 사이 숫자만 입력하세요!");
+                    } else if (num < ranNum) {
+                        System.out.println("======> UP!!   ");
+                        continue;
+                    } else if (num > ranNum) {
+                        System.out.println("======> DOWN!!  ");
+                        continue;
+                    } else if (num == ranNum) {
+                        System.out.println("----축하합니다! 정답입니다 *^^*----");
+                        run = false;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("====   x 숫자만 입력하세요!  ====");
+                    sc.next();
                     continue;
-                } else if (num > ranNum) {
-                    System.out.println("======> DOWN!!  ");
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                     continue;
-                } else if (!sc.hasNextInt()) {
-                    throw new IllegalArgumentException();
                 }
-                System.out.print("한번 더 하시겠습니까? ( 0 입력시 종료 / 아무거나 입력시 한번 더! )");
-                int choice = sc.nextInt();
-                if (choice == 0) {
-                    System.out.println("====  종 료 되 었 습 니 다.  ====");
-                    run1 = false;
-                }
-            }catch (InputMismatchException e){
-                System.out.println("====   x 숫자만 입력하세요!  ====");
-                sc.next();
-                continue;
-            } catch (IllegalArgumentException e){
-                System.out.println("== 1 과 2 중에 하나만 입력하세요 ==");
-                sc.next();
-                continue;
             }
-        }sc.close();
+            System.out.println("한번 더 하시겠습니까? ( end 입력시 종료 / 아무거나 입력시 한번 더  )");
+            String choice = sc.next();
+            if (choice.equals("end")) {
+                System.out.println("====  종 료 되 었 습 니 다.  ====");
+                replay = false;
+            }
+        }
+        sc.close();
     }
 }
